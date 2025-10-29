dcf9f97a6de5b64afda91097ecdb358b7aedba99
del <j.delvina@gmail.com>
2025-10-29 14:57:30 +0700

    update - gitignore

---
+++ Kiki/Screens/StartPage.swift
@@ -0,0 +1,25 @@
+//
+//  StartPage.swift
+//  Kiki
+//
+//  Created by Delvina J on 29/10/25.
+//
+
+import SwiftUI
+
+struct StartPage: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    StartPage()
+}
+
