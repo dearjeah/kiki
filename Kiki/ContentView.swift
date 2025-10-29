99e010e6d2346423dd947263b9357a703c48c50c
del <j.delvina@gmail.com>
2025-10-29 14:47:29 +0700

    Initial Commit

---
+++ Kiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kiki
+//
+//  Created by Delvina J on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kiki/KikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KikiApp.swift
+//  Kiki
+//
+//  Created by Delvina J on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
